3aefd2532058f7cf24e4d2f9006935ab4423fc8a
sheu5777 <sheu5777@gmail.com>
2026-07-13 12:44:03 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='crm-v2';const A=['./','./index.html','./styles.css','./app.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
